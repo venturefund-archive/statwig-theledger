f419d13e3930674ef4265bd2826219a659609cc6
harsha <harsha_d@statwig.com>
2020-03-17 20:38:00 +0530

    shipment created under userservice

--- backend/user_service/routes/api.js
+++ backend/user_service/routes/api.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var authRouter = require("./auth");
-
+var shipmentRouter = require("./shipment")
 var app = express();
 
 app.use("/auth/", authRouter);
+app.use("/shipment",shipmentRouter);
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
