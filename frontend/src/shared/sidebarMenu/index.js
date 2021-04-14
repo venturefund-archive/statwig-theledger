09580f216545c5248eda88a54daa69c6571fc0de
rajpratik8484 <44210415+rajpratik8484@users.noreply.github.com>
2021-04-14 19:44:00 +0530

    sidemenu_panel

--- frontend/src/shared/sidebarMenu/index.js
+++ frontend/src/shared/sidebarMenu/index.js
@@ -1,15 +1,16 @@
+<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 import React from 'react'
 
 import {
   Link
 } from "react-router-dom";
 import Footer from '../footer';
-import HomeIcon from '../../assets/icons/Overview.png';
+import HomeIcon from '../../assets/icons/Overviewselected.png';
 import HomeSelectedIcon from '../../assets/icons/Overviewselected.png';
-import shipIcon from '../../assets/icons/Shippment.png';
+import shipIcon from '../../assets/icons/Shippmentselected.png';
 import InventoryIcon from '../../assets/icons/Inventory.png';
-import trackIcon from '../../assets/icons/Track_Trace.png';
-import NetworkIcon from '../../assets/icons/Network.svg';
+import trackIcon from '../../assets/icons/Track_Traceselected.png';
+import NetworkIcon from '../../assets/icons/blockicon.png';
 import DashboardIcon from '../../assets/icons/Dashboard.png';
 import DashboardSelectedIcon from '../../assets/icons/Dashboardl.png';
 import shipSelectedIcon from '../../assets/icons/Shippmentselected.png';
@@ -19,7 +20,7 @@ import NetworkSelectedIcon from '../../assets/icons/NetworkSelected.svg';
 import UtilitiesSelected from '../../assets/icons/utilitieswhite.png';
 import UtilitiesIcon from '../../assets/icons/utilitiesblue.png';
 import OrderSelectedIcon from '../../assets/icons/orderSelected.png';
-import OrderIcon from '../../assets/icons/Orders.png';
+//import OrderIcon from '../../assets/icons/Orders.png';
 
 import './style.scss'
 const SideBar = ({
@@ -30,42 +31,43 @@ const SideBar = ({
   return (
     <div className="sidebar">
       <ul >
-        <li className={(url === '/overview') ? 'active' : 'inactive'}>
-          <Link to="/overview">
+        <li className={(url === '/overview') ? 'active' : ''}>
+          <Link to="/overview" className="d-inline-block"> 
+          
             <img src={(url === '/overview') ? HomeSelectedIcon : HomeIcon} alt="Overview" />
-            <span>Overview</span>
+            <span className="ml-2">  Overview</span>
           </Link>
         </li>
 
-        <li className={(url === '/orders' || url === '/neworder') ? '' : ''}>
-          <Link to="/orders">
-            <img src={(url === '/orders' || url === '/neworder')  ? OrderSelectedIcon : OrderIcon} alt="Orders" />
-            <span>Orders</span>
+        <li className={(url === '/orders' || url === '/neworder') ? 'active' : ''}>
+          <Link to="/orders" className="d-inline-block">
+            <img src={OrderSelectedIcon } alt="Orders" />
+            <span className="ml-2">  Orders </span>
           </Link>
         </li>
 
         <li className={(url === '/inventory'|| url === '/newinventory'|| url === '/addproduct'|| url === '/productlist/all') ? 'active' : ''}>
-          <Link to="/inventory">
-            <img src={(url === '/inventory' || url === '/newinventory' || url === '/addproduct'|| url === '/productlist/all') ? InventorySelectedIcon : InventoryIcon} alt="Inventory" />
-            <span>Inventory</span>
+          <Link to="/inventory" className="d-inline-block">
+            <img src={InventorySelectedIcon} alt="Inventory" />
+            <span className="ml-2">  Inventory</span>
           </Link>
         </li>
         <li className={(url === '/shipments' || url === '/newshipment'|| url === '/transactionHistory') ? 'active' : ''}>
-          <Link to="/shipments">
+          <Link to="/shipments" className="d-inline-block">
             <img src={(url === '/shipments' || url === '/newshipment'|| url === '/transactionHistory')  ? shipSelectedIcon : shipIcon} alt="Shippment" />
-            <span>Shipments</span>
+            <span className="ml-2"> Shipments</span>
           </Link>
         </li>
          <li className={(url === '/trackAndTrace') ? 'active' : ''}>
-          <Link to="/trackAndTrace">
+          <Link to="/trackAndTrace" className="d-inline-block">
             <img src={(url === '/trackAndTrace') ? trackSelectedIcon : trackIcon} alt="Track & Trace" />
-            <span>Track & Trace</span>
+            <span className="ml-2" > Track & Trace</span>
           </Link>
         </li>
         <li className={(url === '/dashboard')  ? 'active' : ''}>
-          <Link to="/dashboard">
+          <Link to="/dashboard" className="d-inline-block">
             <img src={(url === '/dashboard')  ? NetworkSelectedIcon : NetworkIcon} alt="Shippment" />
-            <span>Network</span>
+            <span className="ml-2"> Network</span>
           </Link>
         </li>
       </ul>
