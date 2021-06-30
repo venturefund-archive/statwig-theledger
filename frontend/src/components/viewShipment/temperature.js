e30a71d2bb8854b21214e0c3542f350c56b33162
santosh kumar m <santu279@gmail.com>
2021-06-30 09:22:58 +0530

    added temperature graph with mock response

--- frontend/src/components/viewShipment/temperature.js
+++ frontend/src/components/viewShipment/temperature.js
@@ -4,18 +4,38 @@ import 'chart.js';
 import { getTemperature } from '../../actions/shipmentActions';
 //import './style.scss'
 
-
+const mockTemperatureData = { data: [
+  { "temp": { "temp": "45", "UnixTimeStamp": "1625010812" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "56", "UnixTimeStamp": "1625020813" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "47", "UnixTimeStamp": "1625050814" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "58", "UnixTimeStamp": "1625020815" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "49", "UnixTimeStamp": "1625060816" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "60", "UnixTimeStamp": "1625020817" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "51", "UnixTimeStamp": "1625050819" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "42", "UnixTimeStamp": "1625020820" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "63", "UnixTimeStamp": "1625050821" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }, 
+  { "temp": { "temp": "24", "UnixTimeStamp": "1625020822" }, "humidity": { "humidity": "34", "UnixTimeStamp": "12345678z" }, "battery": { "battery": "67%", "UnixTimeStamp": "123456z" } }] };
 
 const Chart = () => {
   const [temp, setTemp] = useState({})
 
+  const formatUnixTimeStamp = (timeStamp) => {
+    return new Date(timeStamp * 1e3).toISOString().slice(-13, -5);
+  }
+
+  const prepareTemperatureData = (temperatureData) => {
+    return temperatureData.reduce((obj, item) => (obj[item.temp.temp] = formatUnixTimeStamp(item.temp['UnixTimeStamp']), obj), {});
+  }
 
 
   useEffect(() => {
     const interval = setInterval(() => {
       async function fetchData() {
-        const result = await getTemperature();
-        setTemp(result.data)
+        // const result = await getTemperature();
+        const result = mockTemperatureData;
+        console.log("data: ", prepareTemperatureData(result.data));
+        // setTemp(result.data)
+        setTemp(result.data.length > 0 ? prepareTemperatureData(result.data) : {});
       }
       fetchData();
     }, 5000);
