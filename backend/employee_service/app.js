3ad3a37a1886a211fbc79bc963a2c56749e9e05e
Ravilochan <ravilochan2000@gmail.com>
2021-09-06 05:53:06 +0530

    Express Status Monitor Added to Employee Service

--- backend/employee_service/app.js
+++ backend/employee_service/app.js
@@ -1,21 +1,18 @@
+require("dotenv").config();
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-require("dotenv").config();
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
 const apiResponse = require("./helpers/apiResponse");
 const cors = require("cors");
 const swaggerUi = require('swagger-ui-express');
 const openApiDocumentation = require('./openApiDocumentation');
-// const {getFileStream} = require("./helpers/s3");
+const statusMonitor = require('express-status-monitor')({title:"Employee Service",path: '/usermanagement/api/status'});
 // DB connection
 const MONGODB_URL = process.env.MONGODB_URL;
 const mongoose = require("mongoose");
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true ,useUnifiedTopology: true, useFindAndModify: false,useCreateIndex: true }).then(() => {
-	//don't show the log when it is test
 	if(process.env.NODE_ENV !== "test") {
 		console.log("Connected to %s", MONGODB_URL);
 		console.log("Employee Service is running ... \n");
@@ -29,13 +26,13 @@ const db = mongoose.connection;
 
 var app = express();
 
-//don't show the log when it is test
 if(process.env.NODE_ENV !== "test") {
 	app.use(logger("dev"));
 }
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(statusMonitor.middleware);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
 //To allow cross-origin requests
