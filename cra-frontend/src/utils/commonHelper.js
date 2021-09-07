d7b406f972bb5505cd9f0790420a4b971d3b47db
Ravilochan <ravilochan2000@gmail.com>
2021-09-07 14:09:41 +0530

    Fixing errors

--- cra-frontend/src/utils/commonHelper.js
+++ cra-frontend/src/utils/commonHelper.js
@@ -1,16 +1,30 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
 export const getAddress = (address) => {
-  return address.firstLine +" "+ (address.secondLine ? address.secondLine : "") +" "+ address.city +" "+ address.state +" "+ address.zipCode +" "+ address.country;
-}
+  return (
+    address.firstLine +
+    " " +
+    (address.secondLine ? address.secondLine : "") +
+    " " +
+    address.city +
+    " " +
+    address.state +
+    " " +
+    address.zipCode +
+    " " +
+    address.country
+  );
+};
 
 export const isType = (type, val) => {
-  return !!(val.constructor && val.constructor.name.toLowerCase() === type.toLowerCase());
-}
+  return !!(
+    val.constructor && val.constructor.name.toLowerCase() === type.toLowerCase()
+  );
+};
 
-export const isAuthenticated = permission => {
-  const user = useSelector(state => {
+export const isAuthenticated = (permission) => {
+  const user = useSelector((state) => {
     return state.user;
   });
   return user.permissions.permissions.indexOf(permission) > -1 ? true : false;
-}
+};
