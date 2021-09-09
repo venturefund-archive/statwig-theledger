663ec49c35bcaf66a366b6b0165229df3870b1c3
srtuhi-28 <sruthiailuri34@gmail.com>
2021-09-09 01:25:50 -0700

    errorchanges

--- frontend/src/components/NoMatch.js
+++ frontend/src/components/NoMatch.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import logo from '../assets/brands/error.png'
 
 const NoMatch = () => (
   <div>
+       <img src={logo} alt="Logo" />
     <center>
-      <h1 style={{ color:'red' }}>
-        Error: NO MATCH
-      </h1>
-      <p>
-        <i><a href="/login">Return to login page</a></i>
-      </p>
+    <h1>404 - Not Found!</h1>
+    <Link to="/">
+      Go Home
+    </Link>
     </center>
   </div>
 )
