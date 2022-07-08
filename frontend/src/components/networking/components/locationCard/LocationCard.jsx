c624f8961207c5d3b05c3e214ddc46df043be2cd
Priya-Patel31 <patel3112priya@gmail.com>
2022-07-08 17:27:19 +0530

    location-card

--- frontend/src/components/networking/components/locationCard/LocationCard.jsx
+++ frontend/src/components/networking/components/locationCard/LocationCard.jsx
@@ -3,30 +3,31 @@ import "./locationCard.scss";
 
 const LocationCard = () => {
   return (
-    <div className="location-cards-container mi-flex-sb">
+    <div className="location-cards-container">
       <div className="location-card-container loc-card">
         <div className="location-card-section1">
-          <div className="loction-small-box">
+          <div className="location-small-box mb-1">
             <i className="fa-solid fa-location-crosshairs location-icon"></i>
           </div>
-          <p className="">My Location</p>
+          <p className="color-white location-card-heading">My Location</p>
         </div>
         <div className="location-card-section2">
-          <p className="">100</p>
-          <p className="">ABC Manufacturer</p>
+          <p className="color-white location-card-section2-heading">100</p>
+          <p className="color-white location-card-section2-subtext">
+            ABC Manufacturer
+          </p>
         </div>
       </div>
 
       <div className="partner-card-container loc-card">
         <div className="location-card-section1">
-          <div className="location-icon">
-            <i className="fa-solid fa-location-crosshairs"></i>
+          <div className="location-small-box mb-1">
+          <i className="fa-solid fa-map-location location-icon"></i>
           </div>
-          <p className="">My Location</p>
+          <p className="color-white partner-card-heading">Partner Location</p>
         </div>
         <div className="location-card-section2">
-          <p className="">100</p>
-          <p className="">ABC Manufacturer</p>
+          <p className="color-white location-card-section2-heading">2100</p>
         </div>
       </div>
     </div>
