5a941b3fba58e13626d21150fa6bbd4d3c7b87c8
imarjun <imarjunmuthusmay@gmail.com>
2022-07-15 16:53:52 +0530

    Flow Minor Update

--- frontend/src/components/networking/Networking.jsx
+++ frontend/src/components/networking/Networking.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "./Networking.scss";
 import NetworkMap from "./networkMap/NetworkMap";
 import Reports from "./reports/Reports";
-import Checkbox from "@mui/material/Checkbox";
 import NetworkDashboard from "./networkDashboard/NetworkDashboard";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
