fdc24a3137f467a8e66d034b17de55b9be3a1d89
@imarjun <imarjunmuthusamy@gmail.com>
2022-07-21 14:08:10 +0530

    Redesign Track & Trace

---
+++ frontend/src/components/tracking/chain-of-custody/ChainofCustody.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ChainofCustody() {
+  return <div>Chain of Custody</div>;
+}
---
+++ frontend/src/components/tracking/current-location/CurrentLocation.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CurrentLocation() {
+  return <div>Current Location</div>;
+}
