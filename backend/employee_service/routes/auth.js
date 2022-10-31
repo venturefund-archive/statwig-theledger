c326be6cb8da05210d158fa0b1bf2f50958f7b88
Sravanthi <lakshmisravanthi300@gmail.com>
2022-10-31 09:00:21 +0000

    attempt to resolve conflicts

--- backend/employee_service/routes/auth.js
+++ backend/employee_service/routes/auth.js
@@ -235,7 +235,6 @@ router.post("/deleteProfilePicture", AuthController.deleteProfilePicture);
  *          description: Image uploaded successfully
 */ 
 router.post("/upload", upload.single("profile"), AuthController.uploadImage);
-<<<<<<< HEAD
 // routes/auth.js
 /**
  * @openapi
@@ -301,8 +300,6 @@ router.post("/assignProductConsumer", AuthController.assignProductConsumer);
  *           description: Warehouse added successfully
 */ 
 
-=======
->>>>>>> a84d2b3267603bc84e097adfa070de9b19348a1f
 router.post("/addWarehouse", AuthController.addWarehouse);
 // routes/auth.js
 /**
