5027a4ffd17276127f90df1da1cc92774d6ca4d4
Ravilochan <ravilochan2000@gmail.com>
2023-01-18 22:03:25 +0530

    checkbox to radio

--- frontend/src/components/newShipment/table/editRow.js
+++ frontend/src/components/newShipment/table/editRow.js
@@ -44,8 +44,8 @@ const EditRow = (props) => {
 		img4: <img src={date} width="15" height="15" alt="" />,
 		img5: <img src={date} width="15" height="15" alt="" />,
 		img6: <img src={Quantity} width="20" height="15" alt="" />,
-  };
-  
+	};
+
 	const [editButtonStatus, setEditButtonStatus] = useState(false);
 	const [changebtn, setbtn] = useState(false);
 	const [addnew] = useState(!props.category);
@@ -64,8 +64,8 @@ const EditRow = (props) => {
 	const onEditClick = (e) => {
 		setDisabled(!disabled);
 		setEditButtonStatus(true);
-  };
-  
+	};
+
 	const editBatchSelected = (index, type, value) => {
 		let buffer = [...batches];
 		buffer[index][type] = value;
@@ -108,9 +108,9 @@ const EditRow = (props) => {
 
 	const updateQuantity = () => {
 		setQuantityChecker(0);
-  };
+	};
 
-  useEffect(() => {
+	useEffect(() => {
 		if (
 			check === "0" &&
 			quantityChecker === 1 &&
@@ -146,11 +146,11 @@ const EditRow = (props) => {
 				updateQuantity();
 			}
 		}
-	}, [quantityChecker, prod, productsList]);
-  
-  function existsInShipment(prod) {
+	}, [quantityChecker, prod, productsList, check, props?.product, handleQuantityChange, index]);
+
+	function existsInShipment(prod) {
 		if (props?.product?.length) {
-      for(let i=0; i<props.product.length; ++i) {
+			for (let i = 0; i < props.product.length; ++i) {
 				let currProd = props.product[i];
 				if (
 					prod.productId === currProd.id &&
@@ -158,7 +158,7 @@ const EditRow = (props) => {
 				) {
 					return { exists: true, quantity: currProd.productQuantity };
 				}
-      }
+			}
 		}
 		return { exists: false };
 	}
@@ -167,15 +167,14 @@ const EditRow = (props) => {
 		handleBatchChange(batch.bnp, index, [batch]);
 		handleQuantityChange(batch.quant, index);
 		// closeModal()
-  }
-  
+	}
+
 	async function fetchBatches(prod, index) {
 		const today = new Date();
 		setSelectedIndex(index);
 		setModelProduct(prod);
 		let res = await axios.get(
-			`${config().fetchBatchesOfInventory}?productId=${
-				prod.id ? prod.id : prod.productID
+			`${config().fetchBatchesOfInventory}?productId=${prod.id ? prod.id : prod.productID
 			}&wareId=${warehouseID}`,
 		);
 		let buffer = res.data.data;
@@ -199,7 +198,7 @@ const EditRow = (props) => {
 		});
 		setBatches(buffer);
 	}
-  
+
 	const numbersOnly = (e) => {
 		// Handle paste
 		let key;
@@ -215,18 +214,18 @@ const EditRow = (props) => {
 			e.returnValue = false;
 			if (e.preventDefault) e.preventDefault();
 		}
-  };
-  
+	};
+
 	const handleChange = (value) => {
 		setSelectedBatch(value);
-  };
-  
+	};
+
 	const setQuantity = (value) => {
 		let buffer = selectedBatch;
 		buffer.quant = value;
 		setSelectedBatch(buffer);
-  };
-  
+	};
+
 	const editQuantity = (value, index) => {
 		let buffer = [...batches];
 		if (parseInt(value) > parseInt(buffer[index].immutableQuantity)) {
@@ -237,7 +236,7 @@ const EditRow = (props) => {
 
 		setBatches(buffer);
 		setQuantity(value);
-  };
+	};
 
 	return (
 		<div className="row ml-3 mr-1">
@@ -262,12 +261,12 @@ const EditRow = (props) => {
 										}
 										value={
 											(prod.type === undefined && prod.productCategory === undefined) ||
-											prod.id === undefined
+												prod.id === undefined
 												? null
 												: {
-														value: prod.id,
-														label: prod.type,
-												  }
+													value: prod.id,
+													label: prod.type,
+												}
 										}
 										defaultInputValue={prod.type}
 										onChange={(v) => {
@@ -332,7 +331,7 @@ const EditRow = (props) => {
 					)}
 				</div>
 
-				<div className="col tcell text-center justify-content-center p-2" style={{ position: "relative"}}>
+				<div className="col tcell text-center justify-content-center p-2" style={{ position: "relative" }}>
 					<div className="">
 						<input
 							className="form-control text-center"
@@ -351,17 +350,17 @@ const EditRow = (props) => {
 						/>
 					</div>
 					<div
-					className="title recived-text align-self-center"
-					style={{ position: "absolute", right: "16px", top:"16px" }}
-				>
-					{prod.unitofMeasure && prod.unitofMeasure.name ? (
-						<div>{prod.unitofMeasure.name}</div>
-					) : (
-						<div className="placeholder_id">{t("unit")}</div>
-					)}
+						className="title recived-text align-self-center"
+						style={{ position: "absolute", right: "16px", top: "16px" }}
+					>
+						{prod.unitofMeasure && prod.unitofMeasure.name ? (
+							<div>{prod.unitofMeasure.name}</div>
+						) : (
+							<div className="placeholder_id">{t("unit")}</div>
+						)}
+					</div>
 				</div>
-				</div>
-				
+
 
 				<div className="col tcell text-center justify-content-center p-2">
 					<div className="">
@@ -425,8 +424,9 @@ const EditRow = (props) => {
 															}}
 														>
 															<input
+																name="productRadio"
 																className="txt2 ml-3"
-																type="checkbox"
+																type="radio"
 																id={index}
 																onChange={(e) => {
 																	handleChange({
@@ -459,7 +459,7 @@ const EditRow = (props) => {
 																style={{ position: "relative", left: "8%" }}
 															>
 																{product.attributeSet.mfgDate &&
-																product.attributeSet.mfgDate.length > 0
+																	product.attributeSet.mfgDate.length > 0
 																	? formatDate(product.attributeSet.mfgDate)
 																	: "-"}
 															</div>
@@ -468,7 +468,7 @@ const EditRow = (props) => {
 																style={{ position: "relative", left: "8%" }}
 															>
 																{product.attributeSet.expDate &&
-																product.attributeSet.expDate.length > 0
+																	product.attributeSet.expDate.length > 0
 																	? formatDate(product.attributeSet.expDate)
 																	: "-"}
 															</div>
