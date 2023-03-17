020a962e60b1c74fbc3021608bf730605173c97d
Ubuntu <ubuntu@ip-172-31-0-71.ap-south-1.compute.internal>
2023-03-17 11:22:28 +0000

    Abinbev Prod Server build

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const TEST_SERVER_URL = 'https://test.vaccineledger.com';
+export const TEST_SERVER_URL = 'https://demo.statledger.io';
 export const TEST_SERVER_URL_FOR_ROAMBEE = "https://integrations.vaccineledger.com";
 export const PROD_SERVER_URL = 'https://vaccineledger.com';
 export const ABINBEVPROD_SERVER_URL = 'https://abinbev.statledger.io';
