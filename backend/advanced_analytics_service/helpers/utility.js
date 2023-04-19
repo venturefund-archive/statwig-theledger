d0623a5d3575a16de90978c7e728ac96ad07b994
Ravilochan <ravilochan2000@gmail.com>
2023-04-19 09:11:33 +0530

    advanced_analytics - Redis update

--- backend/advanced_analytics_service/helpers/utility.js
+++ backend/advanced_analytics_service/helpers/utility.js
@@ -1,43 +1,43 @@
-const { promisify } = require("util");
 const redis = require("redis");
 const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT || 6379,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+  },
   password: process.env.REDIS_PASSWORD,
 });
+client.connect(); // Promise
 client.on("connect", () => {
   console.log("Connected to Redis");
 });
 client.on("error", (err) => {
-  console.log("Error " + err);
+  console.log("Redis Error " + err);
 });
-const scanAsync = promisify(client.scan).bind(client);
-const unlinkAsync = promisify(client.unlink).bind(client);
 
 exports.batchDelKeysByPattern = async (pattern) => {
   try {
     let cursor = '0';
     let deletedKeysCount = 0;
     do {
-      const batch = await scanAsync(cursor, 'MATCH', pattern, 'COUNT', '100');
-      if(batch && batch.length) {
+      const batch = await client.SCAN(cursor, 'MATCH', pattern, 'COUNT', '100');
+      if (batch && batch.length) {
         cursor = batch[0];
         deletedKeysCount += batch[1].length;
-        if(batch[1].length)
-          await unlinkAsync(batch[1]);
+        if (batch[1].length)
+          await client.UNLINK(batch[1]);
       }
-    } while(cursor !== '0');
+    } while (cursor !== '0');
     return deletedKeysCount;
-  } catch(err) {
+  } catch (err) {
     throw new Error("Error in clearing cache " + err);
   }
 };
 
 exports.randomNumber = function (length) {
-  var text = "";
-  var possible = "123456789";
-  for (var i = 0; i < length; i++) {
-    var sup = Math.floor(Math.random() * possible.length);
+  let text = "";
+  const possible = "123456789";
+  for (let i = 0; i < length; i++) {
+    const sup = Math.floor(Math.random() * possible.length);
     text += i > 0 && sup == i ? "0" : possible.charAt(sup);
   }
   return Number(text);
