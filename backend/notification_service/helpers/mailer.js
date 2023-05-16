fed683adb34e941875a58747119eecff6a370180
Ravilochan <ravilochan2000@gmail.com>
2023-05-16 10:20:10 +0530

    secure SSL not providded

--- backend/notification_service/helpers/mailer.js
+++ backend/notification_service/helpers/mailer.js
@@ -3,7 +3,7 @@ const { emailBodyGenerator } = require("../templates");
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
   port: process.env.EMAIL_SMTP_PORT,
-  secure: process.env.EMAIL_SMTP_SECURE,
+  // secure: process.env.EMAIL_SMTP_SECURE, // lack of ssl commented this. You can uncomment it.
   auth: {
     user: process.env.EMAIL_SMTP_USERNAME,
     pass: process.env.EMAIL_SMTP_PASSWORD,
