2108759d5144646873b583d62e81b02e58e0391c
Ravilochan <ravilochan2000@gmail.com>
2023-05-26 04:37:53 +0530

    apiKey middleware

--- backend/rewards_service/middlewares/jwt.js
+++ backend/rewards_service/middlewares/jwt.js
@@ -1,9 +1,11 @@
 const jwt = require("jsonwebtoken");
 const apiResponse = require("../helpers/apiResponse");
+const RewardConfigModel = require("../models/RewardConfigModel");
 const JWT_SECRET = process.env.JWT_SECRET;
 const REWARDS_AUTH = process.env.REWARDS_AUTH || false;
 
-module.exports = (req, res, next) => {
+// Middleware to handle user authentication and authorization
+const authUser = (req, res, next) => {
   try {
     if (REWARDS_AUTH) {
       next();
@@ -32,4 +34,26 @@ module.exports = (req, res, next) => {
     console.log(err);
     return apiResponse.errorResponse(res, "Auth Error");
   }
+};
+
+const apiKeyAuth = (req, res, next) => {
+  try {
+    if (REWARDS_AUTH) {
+      next();
+    } else {
+      const { appId, apiKey } = req.body;
+      const keyExists = RewardConfigModel.findOne({ appId, apiKeys: { $in: [apiKey] } });
+      if (keyExists) next();
+      else return new Error({ message: "API Key not found" })
+    }
+  } catch (err) {
+    console.log(err);
+    return apiResponse.errorResponse(res, err?.message || "API Key Error");
+  }
+
+};
+
+module.exports = {
+  authUser,
+  apiKeyAuth
 };
