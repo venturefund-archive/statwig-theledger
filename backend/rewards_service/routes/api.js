82170bb6f1df129a887bd838f3fd36111c24bf52
Ravilochan <ravilochan2000@gmail.com>
2023-05-26 08:34:14 +0530

    updated routes

--- backend/rewards_service/routes/api.js
+++ backend/rewards_service/routes/api.js
@@ -3,7 +3,13 @@ const rewardsController = require("../controllers/RewardsController");
 const router = express.Router();
 
 router.get("/rewards", rewardsController.userRewards)
-router.post("/addReward", rewardsController.addReward)
+router.post("/rewards", rewardsController.addReward)
+router.get("/rewards/:id", rewardsController.viewReward)
+router.put("/rewards/:id", rewardsController.updateReward)
+router.delete("/rewards/:id", rewardsController.deleteReward);
 router.get("/rewardsList", rewardsController.listOfRewards)
+router.get("/redemptions", rewardsController.listOfRedemptions)
+router.post("/redemptions", rewardsController.addRedemption)
+router.post("/registerApp", rewardsController.registerApp)
 
 module.exports = router;
