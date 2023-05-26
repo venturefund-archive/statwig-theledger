62f717dfe8a142a9f424f39ef1c4fc1f9a19319e
Ravilochan <ravilochan2000@gmail.com>
2023-05-26 08:35:28 +0530

    Rewards Controller

--- backend/rewards_service/controllers/RewardsController.js
+++ backend/rewards_service/controllers/RewardsController.js
@@ -1,28 +1,18 @@
 const RewardModel = require("../models/RewardModel");
-const RewardConfigModel = require("../models/RewardConfigModel");
+const RedeemModel = require("../models/RedeemModel");
+const RewardUserModel = require("../models/RewardUserModel");
 const apiResponse = require("../helpers/apiResponse");
-const auth = require("../middlewares/jwt");
+const { asyncHandler, authUser, apiKeyAuth, roleAuth } = require("../middlewares/jwt");
+const RewardConfigModel = require("../models/RewardConfigModel");
+const cuid = require("cuid");
 
 exports.userRewards = [
-    auth,
+    authUser,
     async function (req, res) {
         try {
-            const { role } = req.user;
-            const isRoleAvailable = await RewardConfigModel.findOne({})
-        }
-        catch (err) {
-            console.log(err);
-            return apiResponse.errorResponse(err);
-        }
-    }
-]
-
-exports.addReward = [
-    auth,
-    async function (req, res) {
-        try {
-            const { role } = req.user;
-            const isRoleAvailable = await RewardConfigModel.findOne({})
+            const { appId } = req.headers;
+            const rewards = await RewardUserModel.findOne({ appId, userId: req.user.id })
+            return apiResponse.successResponseWithData(res, "User Rewards", rewards)
         }
         catch (err) {
             console.log(err);
@@ -32,15 +22,126 @@ exports.addReward = [
 ]
 
 exports.listOfRewards = [
-    auth,
+    authUser,
     async function (req, res) {
         try {
-            const { role } = req.user;
-            const isRoleAvailable = await RewardConfigModel.findOne({})
+            const { appId } = req.headers;
+            const rewards = await RewardModel.find({ appId, userId: req.user.id })
+            return apiResponse.successResponseWithData(res, "List of Rewards", rewards)
         }
         catch (err) {
             console.log(err);
             return apiResponse.errorResponse(err);
+        }
+    }
+]
+
+exports.viewReward = [
+    authUser,
+    async function (req, res) {
+        try {
+            const { id } = req.params;
+            const reward = await RewardModel.findById(id)
+            return apiResponse.successResponseWithData(res, "View Reward", reward)
+        }
+        catch (err) {
+            console.log(err);
+            return apiResponse.errorResponse(err);
+        }
+    }
+]
+
+exports.updateReward = [
+    asyncHandler(apiKeyAuth),
+    asyncHandler(roleAuth),
+    async function (req, res) {
+        try {
+            const { id } = req.params;
+            const reward = await RewardModel.findByIdAndUpdate(id, { $set: req.body });
+            return apiResponse.successResponseWithData(res, "Update Reward", reward)
+        }
+        catch (err) {
+            console.log(err);
+            return apiResponse.errorResponse(err);
+        }
+    }
+]
+
+exports.deleteReward = [
+    asyncHandler(apiKeyAuth),
+    asyncHandler(roleAuth),
+    async function (req, res) {
+        try {
+            const { id } = req.params;
+            const reward = await RewardModel.findByIdAndDelete(id);
+            return apiResponse.successResponseWithData(res, "Delete Reward", reward)
+        }
+        catch (err) {
+            console.log(err);
+            return apiResponse.errorResponse(err);
+        }
+    }
+]
+
+exports.addReward = [
+    asyncHandler(apiKeyAuth),
+    asyncHandler(roleAuth),
+    async function (req, res) {
+        try {
+            const { appId } = req.headers;
+            const reward = new RewardModel(...req.body, appId);
+            await reward.save();
+            return apiResponse.successResponseWithData(res, "Reward Added", reward)
+        }
+        catch (err) {
+            console.log(err);
+            return apiResponse.errorResponse(err);
+        }
+    }
+]
+
+exports.addRedemption = [
+    asyncHandler(apiKeyAuth),
+    asyncHandler(roleAuth),
+    async function (req, res) {
+        try {
+            const { appId } = req.headers;
+            const reward = new RedeemModel(...req.body, appId);
+            await reward.save();
+            return apiResponse.successResponseWithData(res, "Reward Added Successfully", reward)
+        }
+        catch (err) {
+            console.log(err);
+            return apiResponse.errorResponse(err);
+        }
+    }
+]
+
+exports.listOfRedemptions = [
+    authUser,
+    async function (req, res) {
+        try {
+            const { appId } = req.headers;
+            const rewards = await RedeemModel.find({ appId, userId: req.user.id })
+            return apiResponse.successResponseWithData(res, "List of Redemptions", rewards)
+        }
+        catch (err) {
+            console.log(err);
+            return apiResponse.errorResponse(err);
+        }
+    }
+]
+
+exports.registerApp = [
+    async function (req, res) {
+        try {
+            const newApp = new RewardConfigModel({ ...req.body, appId: cuid(), apiKeys: ["API" + cuid()] });
+            await newApp.save();
+            return apiResponse.successResponseWithData(res, "App Register Successfully", newApp);
+        }
+        catch (err) {
+            console.log(err);
+            return apiResponse.errorResponse(err)
         }
     }
 ]
