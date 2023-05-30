8f99661c00dab38cb06e387429180d64459607cd
Ravilochan <ravilochan2000@gmail.com>
2023-05-30 16:56:09 +0530

    rewards helper to make API call

---
+++ backend/last_mile_service/helpers/rewards.js
@@ -0,0 +1,62 @@
+const axios = require('axios');
+const REWARDS_SERVICE_URL = process.env.REWARDS_SERVICE_URL || 'https://test.vaccineledger.com/rewardmanagement/api/';
+const REWARDS_API_KEY = process.env.REWARDS_API_KEY || "testApiKey";
+// Queue to store failed requests
+const requestQueue = [];
+
+// Function to send HTTP request
+async function addReward(data) {
+    try {
+        axios.defaults.headers.common['x-api-key'] = REWARDS_API_KEY;
+        await axios.post(REWARDS_SERVICE_URL + "rewards", data);
+        console.log('Request sent successfully');
+    } catch (error) {
+        console.error('Failed to send request:', error);
+        // Add request to the queue for later retry
+        requestQueue.push(data);
+    }
+}
+
+// Function to retry failed requests from the queue
+async function retryFailedRequests() {
+    while (requestQueue.length > 0) {
+        const requestData = requestQueue.shift();
+        await addReward(requestData);
+    }
+}
+
+// Start retrying failed requests when the microservice is available
+microserviceAvailabilityCheck()
+    .then(() => {
+        retryFailedRequests();
+    })
+    .catch((error) => {
+        console.error('Failed to check microservice availability:', error);
+    });
+
+// Function to periodically check microservice availability
+async function microserviceAvailabilityCheck() {
+    try {
+        // Check microservice availability (e.g., ping or health endpoint)
+        const response = await axios.get('microservice_health_url');
+        if (response.status === 200) {
+            console.log('Microservice is available');
+            return;
+        }
+    } catch (error) {
+        console.error('Microservice is unavailable:', error);
+    }
+
+    // Retry after a delay
+    await delay(360000);
+    await microserviceAvailabilityCheck();
+}
+
+// Helper function to introduce a delay using promises
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+module.exports = {
+    addReward
+}
