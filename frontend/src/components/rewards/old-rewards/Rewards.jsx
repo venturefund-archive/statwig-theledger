9661c8ea8a3ff1f4888a76b369e6fe80aa4225b2
Ravilochan <ravilochan2000@gmail.com>
2023-05-31 15:56:23 +0530

    rewards UI update

--- frontend/src/components/rewards/old-rewards/Rewards.jsx
+++ frontend/src/components/rewards/old-rewards/Rewards.jsx
@@ -4,58 +4,58 @@ import "./Rewards.css";
 
 export default function Rewards({ t }) {
   return (
-    <section className="reward_main_layout">
-      <div className="reward_content_area">
-        <div className="reward_header">
+    <section className='reward_main_layout'>
+      <div className='reward_content_area'>
+        <div className='reward_header'>
           <h1
             style={{ paddingBottom: "10px" }}
-            className="vl-heading-bdr black f-700 mi-reset"
+            className='vl-heading-bdr black f-700 mi-reset'
           >
             Rewards
           </h1>
         </div>
-        <div className="reward_body">
-          <div className="rewards_wrapper">
-            <div className="rewards_card">
-              <img src={Reward} alt="Reward" />
-              <p className="reward_point_counter">2,000</p>
-              <p className="reward_message_ts">Your Points</p>
+        <div className='reward_body'>
+          <div className='rewards_wrapper'>
+            <div className='rewards_card'>
+              <img src={Reward} alt='Reward' />
+              <p className='reward_point_counter'>2,000</p>
+              <p className='reward_message_ts'>Your Points</p>
             </div>
-            <div className="rewards_classification">
-              <div className="reward_points border_added">
-                <p className="reward_heading_ts">Order Reward Points</p>
-                <div className="points_wrap">
-                  <p className="reward_points_ts"> 200</p>
-                  <i class="fa-solid fa-medal"></i>
+            <div className='rewards_classification'>
+              <div className='reward_points border_added'>
+                <p className='reward_heading_ts'>Order Reward Points</p>
+                <div className='points_wrap'>
+                  <p className='reward_points_ts'> 200</p>
+                  <i className='fa-solid fa-medal'></i>
                 </div>
               </div>
-              <div className="reward_points border_added">
-                <p className="reward_heading_ts">Shipment Reward Points</p>
-                <div className="points_wrap">
-                  <p className="reward_points_ts"> 200</p>
-                  <i class="fa-solid fa-medal"></i>
+              <div className='reward_points border_added'>
+                <p className='reward_heading_ts'>Shipment Reward Points</p>
+                <div className='points_wrap'>
+                  <p className='reward_points_ts'> 200</p>
+                  <i className='fa-solid fa-medal'></i>
                 </div>
               </div>
-              <div className="reward_points">
-                <p className="reward_heading_ts">LastMile Reward Points</p>
-                <div className="points_wrap">
-                  <p className="reward_points_ts"> 200</p>
-                  <i class="fa-solid fa-medal"></i>
+              <div className='reward_points'>
+                <p className='reward_heading_ts'>LastMile Reward Points</p>
+                <div className='points_wrap'>
+                  <p className='reward_points_ts'> 200</p>
+                  <i className='fa-solid fa-medal'></i>
                 </div>
               </div>
-              <div className="claim_rewards">
-                <div className="claim_headline">
-                  <h1 className="claim_title_ts">Claim Your Rewards</h1>
-                  <p className="claim_subtitle_ts">
+              <div className='claim_rewards'>
+                <div className='claim_headline'>
+                  <h1 className='claim_title_ts'>Claim Your Rewards</h1>
+                  <p className='claim_subtitle_ts'>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     Eius, laudantium!
                   </p>
                 </div>
-                <div className="claim_actions">
-                  <button className="mi-btn mi-btn-md mi-btn-secondary">
+                <div className='claim_actions'>
+                  <button className='mi-btn mi-btn-md mi-btn-secondary'>
                     Transfer to Bank
                   </button>
-                  <button className="mi-btn mi-btn-md mi-btn-secondary-outline">
+                  <button className='mi-btn mi-btn-md mi-btn-secondary-outline'>
                     Convert to Crypto
                   </button>
                 </div>
--- frontend/src/components/rewards/redeem/Redeem.jsx
+++ frontend/src/components/rewards/redeem/Redeem.jsx
@@ -4,54 +4,54 @@ import "./Redeem.css";
 
 export default function Redeem({ onClose, RedeemOption }) {
   return (
-    <div className="redeem_container">
-      <div className="redeem_header">
-        <h1 className="reward_title_ts">Claim Your Rewards</h1>
+    <div className='redeem_container'>
+      <div className='redeem_header'>
+        <h1 className='reward_title_ts'>Claim Your Rewards</h1>
 
-        <div className="close_btn" onClick={onClose}>
-          <i class="fa-solid fa-xmark"></i>
+        <div className='close_btn' onClick={onClose}>
+          <i className='fa-solid fa-xmark'></i>
         </div>
       </div>
-      <div className="redeem_body">
-        <p className="points_subtitle_ts">
+      <div className='redeem_body'>
+        <p className='points_subtitle_ts'>
           congratulations, You got your points. Please fill the details to
           redeem your rewards
         </p>
         <TextField
           autoFocus
-          margin="dense"
-          id="name"
-          label="Full Name"
-          type="text"
+          margin='dense'
+          id='name'
+          label='Full Name'
+          type='text'
           fullWidth
         />
         <TextField
-          margin="dense"
-          id="name"
-          label="Email Address"
-          type="email"
+          margin='dense'
+          id='name'
+          label='Email Address'
+          type='email'
           fullWidth
         />
         {RedeemOption === "bank" ? (
           <TextField
-            margin="dense"
-            id="name"
-            label="Bank Account Number"
-            type="text"
+            margin='dense'
+            id='name'
+            label='Bank Account Number'
+            type='text'
             fullWidth
           />
         ) : (
           <TextField
-            margin="dense"
-            id="name"
-            label="Wallet Address"
-            type="text"
+            margin='dense'
+            id='name'
+            label='Wallet Address'
+            type='text'
             fullWidth
           />
         )}
       </div>
-      <div className="redeem_footer">
-        <button className="mi-btn mi-btn-md mi-btn-secondary">
+      <div className='redeem_footer'>
+        <button className='mi-btn mi-btn-md mi-btn-secondary'>
           {RedeemOption === "bank"
             ? "Redeem to Bank Account"
             : "Buy cryptocurrency"}
