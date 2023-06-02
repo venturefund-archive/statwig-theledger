650b887dc6468256047453085cfdeb24c46dbc90
Ravilochan <ravilochan2000@gmail.com>
2023-06-02 08:13:25 +0530

    update event logger

--- utils/event_logger/eventLogger.js
+++ utils/event_logger/eventLogger.js
@@ -1,15 +1,15 @@
 const validate = require("./helpers/validation.js");
 const Event = require("./models/EventModal");
 const config = require("./config.js");
-const { addReward } = require("./helpers/rewards.js")
+const { addReward } = require("./helpers/rewards.js");
 const MONGODB_URL = process.env.MONGODB_URL || config.MONGODB_URL;
 const mongoose = require("mongoose");
-mongoose
-  .connect(MONGODB_URL, {
-    keepAlive: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+
+mongoose.connect(MONGODB_URL, {
+  keepAlive: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => {
     if (process.env.NODE_ENV !== "test") {
       console.log("Event Logger Connected to DB");
@@ -21,98 +21,48 @@ mongoose
   });
 
 async function logEvent(data) {
-  if (validate(data)) {
-    try {
-      const rewardData = {
-        eventId: data.eventID,
-        event: data.eventType.primary,
-        eventType: data.eventType.description,
-        eventTime: new Date(),
-        userId: data.actor.actoruserid,
-        userOrgId: data.stackholders.actororg.id,
-        userWarehouseId: data.actorWarehouseId,
-      }
-      await addReward(rewardData);
-      return await Event.findOne(
-        { eventID: data.eventID },
-        async function (err, foundEvent) {
-          if (err) console.log("Error is", err);
-          if (foundEvent != null) {
-            err = {
-              message: "Event exists with same Event ID",
-              code: 500,
-            };
-            console.log(err);
-            return err;
-          } else {
-            const event = new Event({
-              eventID: data.eventID,
-              eventTime: data.eventTime,
-              transactionId: data.transactionId ? data.transactionId : null,
-              eventTypePrimary: data.eventType.primary,
-              eventTypeDesc: data.eventType.description,
-              actorId: data.actor.actorid,
-              actorUserId: data.actor.actoruserid,
-              caId: data.stackholders.ca.id,
-              caName: data.stackholders.ca.name,
-              caAddress: data.stackholders.ca.address,
-              actorOrgId: data.stackholders.actororg.id,
-              actorOrgName: data.stackholders.actororg.name,
-              actorOrgAddress: data.stackholders.actororg.address,
-              actorWarehouseId: data.actorWarehouseId,
-              secondaryOrgId: data.stackholders.secondorg.id,
-              secondaryOrgName: data.stackholders.secondorg.name,
-              secondaryOrgAddress: data.stackholders.secondorg.address,
-              payloadData: data.payload,
-            });
-            return event.save();
-          }
-        }
-      ).clone();
-    } catch (err) {
-      return await Event.findOne(
-        { eventID: data.eventID },
-        async function (err, foundEvent) {
-          if (err) console.log("Error is", err);
-          if (foundEvent != null) {
-            err = {
-              message: "Event exists with same Event ID",
-              code: 500,
-            };
-            console.log(err);
-            return err;
-          } else {
-            const event = new Event({
-              eventID: data.eventID,
-              eventTime: data.eventTime,
-              transactionId: data.transactionId ? data.transactionId : null,
-              eventTypePrimary: data.eventType.primary,
-              eventTypeDesc: data.eventType.description,
-              actorId: data.actor.actorid,
-              actorUserId: data.actor.actoruserid,
-              caId: data.stackholders.ca.id,
-              caName: data.stackholders.ca.name,
-              caAddress: data.stackholders.ca.address,
-              actorOrgId: data.stackholders.actororg.id,
-              actorOrgName: data.stackholders.actororg.name,
-              actorOrgAddress: data.stackholders.actororg.address,
-              actorWarehouseId: data.actorWarehouseId,
-              secondaryOrgId: data.stackholders.secondorg.id,
-              secondaryOrgName: data.stackholders.secondorg.name,
-              secondaryOrgAddress: data.stackholders.secondorg.address,
-              payloadData: data.payload,
-            });
-            return event.save();
-          }
-        }
-      );
+  try {
+    if (!validate(data)) {
+      const err = {
+        message: "Data Invalid: Fields incorrect",
+        code: 500,
+      };
+      return err;
     }
-  } else {
-    const err = {
-      message: "Data Invalid : Fields incorrect",
-      code: 500,
+    const rewardData = {
+      eventId: data.eventID,
+      event: data.eventType.primary,
+      eventType: data.eventType.description,
+      eventTime: new Date(),
+      userId: data.actor.actoruserid,
+      userOrgId: data.stackholders.actororg.id,
+      userWarehouseId: data.actorWarehouseId,
     };
-    return err;
+    await addReward(rewardData);
+    const event = new Event({
+      eventID: data.eventID,
+      eventTime: data.eventTime,
+      transactionId: data.transactionId || null,
+      eventTypePrimary: data.eventType.primary,
+      eventTypeDesc: data.eventType.description,
+      actorId: data.actor.actorid,
+      actorUserId: data.actor.actoruserid,
+      caId: data.stackholders.ca.id,
+      caName: data.stackholders.ca.name,
+      caAddress: data.stackholders.ca.address,
+      actorOrgId: data.stackholders.actororg.id,
+      actorOrgName: data.stackholders.actororg.name,
+      actorOrgAddress: data.stackholders.actororg.address,
+      actorWarehouseId: data.actorWarehouseId,
+      secondaryOrgId: data.stackholders.secondorg.id,
+      secondaryOrgName: data.stackholders.secondorg.name,
+      secondaryOrgAddress: data.stackholders.secondorg.address,
+      payloadData: data.payload,
+    });
+    await event.save();
+    return event;
+  } catch (err) {
+    console.log("EventLogger Error occurred:", err);
   }
 }
 
